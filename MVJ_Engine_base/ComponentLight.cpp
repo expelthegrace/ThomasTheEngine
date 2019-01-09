60ffbdac50c384414d01b30557bd982ecb687350
expelthegrace <32312668+expelthegrace@users.noreply.github.com>
2019-01-09 13:36:31 +0100

    ComponentLight cone nedded to invert initial point

--- MVJ_Engine_base/ComponentLight.cpp
+++ MVJ_Engine_base/ComponentLight.cpp
@@ -21,12 +21,13 @@ ComponentLight::~ComponentLight()
 
 update_status ComponentLight::Update() {
 	position = my_go->transform->globalPosition;
+	
 	direction = (my_go->transform->rotation * initialDirection).Normalized();
 	colorLight = float3(min(colorLight.x * intensity, 1.f), min(colorLight.y * intensity, 1.f), min(colorLight.z * intensity, 1.f));
 
 	const ddVec3 boxColor = { 0.f, 0.8f, 0.8f };
-	//dd::cone(position, direction, boxColor, 0.f * App->GameScale, 0.5f * App->GameScale);
-	dd::line(position, direction, boxColor);
+	dd::cone(position, direction, boxColor, 0.f * App->GameScale, 0.5f * App->GameScale);
+	//dd::line(position, direction, boxColor);
 
 	return UPDATE_CONTINUE;
 }
